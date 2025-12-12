e7332e3526906d975e53e3100783d00bce7be3df
ymhp64t9bz-png <ymhp64t9bz@privaterelay.appleid.com>
2025-12-12 16:10:29 -0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -116,7 +116,7 @@ try:
     
     B2_KEY_ID = os.environ.get("B2_KEY_ID", "00568702c2cbfc60000000001")
     B2_APP_KEY = os.environ.get("B2_APPLICATION_KEY", "K005aP6cXPuBIw6IakBaMHYtXx4VGq")
-    B2_ENDPOINT = os.environ.get("B2_ENDPOINT", "https://s3.us-east-005.backblazeb2.com")
+    B2_ENDPOINT = os.environ.get("B2_ENDPOINT", "https://tree-iad1-0002.secure.backblaze.com/b2_browse_files2.htm?bucketId=76b8a7e0b23c02ec9bbf0c16")
     B2_BUCKET = os.environ.get("B2_BUCKET_NAME", "KortexAI")
     
     if B2_KEY_ID and B2_APP_KEY:
